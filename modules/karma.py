4c7a0e95a785cf45b77ce2aeebda45ec9296ff53
Virgilio BSD <DtxdF@protonmail.com>
2021-05-21 23:22:46 -0400

    *.- Era necesario eliminar los espacios de los nombres de usuarios para los usuarios que los autocompleten *.- Se añadió una nueva funcionalidad para limitar el intervalo para que los usuarios no pudieran causar spam al canal

--- modules/karma.py
+++ modules/karma.py
@@ -1,10 +1,14 @@
+import json
 import re
+import time
 from sopel import module
 from sopel import formatting
 from sopel.formatting import colors
 
 KARMA_KEY = "karma"
+KARMA_KEY_INFO = "karma_info"
 BLOCKLIST_RE = r"^(I|i)nvitado\d+$"
+LIMIT_SECOND = 10
 
 @module.commands("karma")
 @module.example(".karma <Nombre de usuario> [-/+]")
@@ -17,7 +21,7 @@ def karma(bot, trigger):
         bot.reply(formatting.color("¡Sigue la sintaxis!", colors.YELLOW))
         return
     else:
-        nick = nick.lower()
+        nick = nick.lower().strip()
 
     is_sum = trigger.group(4)
     if (is_sum is None or is_sum[:1] == "+"):
@@ -44,6 +48,21 @@ def karma(bot, trigger):
         bot.reply(formatting.color("Este usuario no existe o no se encuentra, intenta otra vez.", colors.RED))
         return
 
+    karma_info = bot.db.get_nick_value(nick, KARMA_KEY_INFO)
+    if (karma_info is not None):
+        karma_info = json.loads(karma_info)
+    else:
+        karma_info = {}
+
+    karma_info["start_karma"] = karma_info.get("start_karma")
+    if (karma_info["start_karma"] is None):
+        karma_info["start_karma"] = time.time()
+    elif (round(time.time()-karma_info["start_karma"]) < LIMIT_SECOND):
+        bot.reply("Todavía no puedes dar karma, porque todavía no se superan los %ds segundos" % (LIMIT_SECOND))
+        return
+    else:
+        karma_info["start_karma"] = time.time()
+
     karma_val = bot.db.get_nick_value(nick, KARMA_KEY, 0)
     karma_val = int(karma_val)
     if (is_sum):
@@ -52,6 +71,7 @@ def karma(bot, trigger):
         karma_val -= 1
 
     bot.db.set_nick_value(nick, KARMA_KEY, karma_val)
+    bot.db.set_nick_value(nick, KARMA_KEY_INFO, json.dumps(karma_info))
     bot.say(formatting.color("%s recibe %s1 de karma de parte de %s. Ahora tiene %d." % (
         nick,
         "+" if (is_sum) else "-",
@@ -64,8 +84,11 @@ def getKarma(bot, trigger):
     """Obtiene el karma de un usuario"""
 
     nick = trigger.group(2) or trigger.nick
-    nick = nick.lower()
+    nick = nick.lower().strip()
 
-    karma_val = bot.db.get_nick_value(nick, KARMA_KEY, 0)
+    karma_val = bot.db.get_nick_value(nick, KARMA_KEY)
+    if (karma_val is None):
+        bot.reply("%s no existe." % (nick))
+        return
 
     bot.say("%s tiene %d de karma." % (nick, karma_val))
